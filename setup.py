7086813a33d9293951112200b9fc6ab24c77bc6b
Bentsi Magidovich <bentsi@continueai.com>
2022-10-30 13:04:34 +0400

    upgrade cryptography version to 36.0.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="pdfservices-sdk",
-    version="1.0.2",
+    version="1.0.2-crypto-upgrade",
     author='Adobe Document Services',
     author_email='extractapi@adobe.com',
     license='Apache2',
